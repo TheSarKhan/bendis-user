d9c5a4bcf441ce6ab4a351345fb34768ad25b577
iamnermin <memmedlinermin205@gmail.com>
2025-06-05 13:38:35 +0400

    add componentscan

--- src/main/java/com/sarkhan/backend/BackendApplication.java
+++ src/main/java/com/sarkhan/backend/BackendApplication.java
@@ -2,11 +2,12 @@ package com.sarkhan.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-
+@ComponentScan(basePackages = {"com.sarkhan.backend.mapper.seller", "com.sarkhan.backend"})
 public class BackendApplication {
 
     public static void main(String[] args) {
