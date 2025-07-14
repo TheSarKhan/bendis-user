2f51daf1793936a1467751a695b80325ec9f099a
Nermin Memmedli <memmedlinermin205@gmail.com>
2025-07-14 12:57:01 +0400

    write getAll and getById response features for order

--- src/main/java/com/sarkhan/backend/service/OrderService.java
+++ src/main/java/com/sarkhan/backend/service/OrderService.java
@@ -2,12 +2,18 @@ package com.sarkhan.backend.service;
 
 import com.sarkhan.backend.dto.order.OrderFilterRequest;
 import com.sarkhan.backend.dto.order.OrderRequest;
+import com.sarkhan.backend.dto.order.OrderResponseDto;
 import com.sarkhan.backend.exception.NotEnoughQuantityException;
 import com.sarkhan.backend.model.order.Order;
 
 import java.util.List;
 
 public interface OrderService {
+    List<OrderResponseDto> getAll(String token);
+
+    OrderResponseDto getById(Long orderId, String token);
+
     String createOrder(OrderRequest orderRequest, String token) throws NotEnoughQuantityException;
-    List<Order> filterOrders(OrderFilterRequest orderFilterRequest,String token);
+
+    List<Order> filterOrders(OrderFilterRequest orderFilterRequest, String token);
 }
